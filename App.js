8edd510bdfa418574cf83cd8e6be011dc847fc2f
Burak Sakallı <buraksakalli06@gmail.com>
2020-01-02 12:39:53 +0300

    artist page done.

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import SplashScreen from './components/Pages/SplashScreen';
 import ArtistPage from './components/Pages/ArtistPage';
+import MainPage from './components/Pages/MainPage';
 
 export default class App extends Component {
   render() {
     return (
-      <SplashScreen />
+      <MainPage />
     )
   }
 }
--- components/Pages/ArtistPage.js
+++ components/Pages/ArtistPage.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, ImageBackground } from 'react-native'
+import { Text, View, StyleSheet, ImageBackground, Dimensions, ScrollView } from 'react-native'
 import Header from '../Header';
 import { Fonts } from '../Fonts';
 
 export default class ArtistPage extends Component {
   render() {
+
+    // Getting screen dimensions
+    const Screen = {
+      height: Math.round(Dimensions.get("window").height),
+      width: Math.round(Dimensions.get("window").width)
+    }
+
     const style = StyleSheet.create({
       container: {
-        flex: 1
+        width: Screen.width + 1,
+        height: Screen.height + 1
       },
       bg: {
-        flex: 1,
-        resizeMode: 'cover',
+        width: Screen.width + 1,
+        height: Screen.height + 1,
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-end'
       },
       info: {
-        marginLeft: 30,
-        marginBottom: 30,
+        paddingLeft: 30,
+        paddingBottom: 30,
+        paddingRight: 40,
       },
       year: {
         fontFamily: Fonts.OggItalic,
@@ -30,18 +39,23 @@ export default class ArtistPage extends Component {
         fontSize: 45,
         color: '#EEEEEE',
       }
-    })
+    });
     return (
-      <View style={style.container}>
-        <Header />
-        <ImageBackground style={style.bg} source={require('../../src/img/artists/rembrandt.jpg')}>
-          <View style={style.info}>
-            <Text style={style.year}>1778-1860</Text>
-            <Text style={style.name}>Rembrandt Peale</Text>
-          </View>
+      <ScrollView>
+        <View style={style.container}>
+          <Header />
+          <ImageBackground style={style.bg} source={require('../../src/img/artists/rembrandt.jpg')}>
+            <View style={style.info}>
+              <Text style={style.year}>1778-1860</Text>
+              <Text style={style.name}>Rembrandt Peale</Text>
+            </View>
 
-        </ImageBackground>
-      </View>
+          </ImageBackground>
+        </View>
+        <View style={style.content}>
+          <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Text>
+        </View>
+      </ScrollView>
     )
   }
 }
